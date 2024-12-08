de33d03431960ee634a1e712a1ec92981e248fe9
Vita <Shatory@gmail.com>
2024-12-08 11:18:19 +0200

    js

--- js/task-4.js
+++ js/task-4.js
@@ -1,48 +1,23 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter((user) => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-};
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
 
-const clients = [
-  {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811,
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821,
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793,
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278,
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951,
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498,
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764,
-  },
-];
+  event.preventDefault();
 
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
+  const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
 
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+});
